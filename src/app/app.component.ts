2df8577814476b26fb7b3e14825a3028f643c296
MarcosBoscariolo <marcosboscariolo@hotmail.com>
2023-01-25 20:09:13 -0300

    added toolbar fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,41 @@
-import { Component, AfterContentInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver } from '@angular/cdk/layout';
+import { fromEvent, map } from 'rxjs';
+
+export const SCROLL_CONTAINER = "mat-sidenav-content";
+export const TEXT_LIMIT = 50;
+export const SHADOW_LIMIT = 100;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterContentInit{
-
+export class AppComponent implements OnInit {
   title = 'Angular-Material';
   openSidenav: boolean = false;
+  popText: boolean = false;
+  applyShadow: boolean = false;
 
   constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit() {
+    const content = document.getElementsByClassName(SCROLL_CONTAINER)[0];
+    fromEvent(content, "scroll")
+      .pipe(
+        map(() => content.scrollTop)
+      )
+      .subscribe({
+        next: (value: number) => {
+          this.determineHeader(value);
+        }
+      });
+  }
+
+  determineHeader(scrollTop: number): void {
+    this.popText = scrollTop >= TEXT_LIMIT;
+    this.applyShadow = scrollTop >= SHADOW_LIMIT;
+  }
 
   ngAfterContentInit() {
     this.breakpointObserver

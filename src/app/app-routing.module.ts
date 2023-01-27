2e902d6c824915405abda4ed5f0a2a561dcc9c39
MarcosBoscariolo <marcosboscariolo@hotmail.com>
2023-01-26 22:40:14 -0300

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormFieldInputComponent } from './components/form-field-input/form-field-input.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inputs', component: FormFieldInputComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,3 +1,4 @@
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -5,7 +6,8 @@ import { MatSliderModule } from '@angular/material/slider';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
-import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { MatListModule } from '@angular/material/list';
+
 
 @NgModule({
   exports: [
@@ -14,7 +16,8 @@ import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
     MatIconModule,
     MatButtonModule,
     MatSidenavModule,
-    MatInputModule
+    MatInputModule,
+    MatListModule
   ],
   providers: [
     {

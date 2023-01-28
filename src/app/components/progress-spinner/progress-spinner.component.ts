547fbe0928134684ecdfd499a5dd2cc70f50719e
MarcosBoscariolo <marcosboscariolo@hotmail.com>
2023-01-28 13:25:49 -0300

    progress case loading waiting

--- src/app/components/progress-spinner/progress-spinner.component.ts
+++ src/app/components/progress-spinner/progress-spinner.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { interval, map, Observable, takeWhile } from 'rxjs';
+import { ProgressBarMode } from '@angular/material/progress-bar';
+import { concat, interval, map, Observable, take, takeWhile, tap } from 'rxjs';
 
 @Component({
   selector: 'app-progress-spinner',
@@ -9,11 +10,21 @@ import { interval, map, Observable, takeWhile } from 'rxjs';
 export class ProgressSpinnerComponent implements OnInit {
 
   loadingPercent: number = 0;
+  loadingWaitPercent: number = 0;
+  mode: ProgressBarMode = "query"
 
   constructor() { }
 
   ngOnInit(): void {
     this.loadingProgress(400).subscribe(i => this.loadingPercent = i);
+    concat(
+      interval(2000)
+      .pipe(
+        take(1),
+        tap(_ => this.mode = "determinate")
+      ),
+      this.loadingProgress(500)
+    ).subscribe(i => this.loadingWaitPercent = i);
   }
 
   loadingProgress(speed: number): Observable<number> {

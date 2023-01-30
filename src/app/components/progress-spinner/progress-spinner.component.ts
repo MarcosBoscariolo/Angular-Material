f92f1fd6ce3a182440944ee82bc598f8b39590fa
MarcosBoscariolo <marcosboscariolo@hotmail.com>
2023-01-30 20:40:26 -0300

    added progress spinner and bar

--- src/app/components/progress-spinner/progress-spinner.component.ts
+++ src/app/components/progress-spinner/progress-spinner.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProgressBarMode } from '@angular/material/progress-bar';
-import { concat, interval, map, Observable, take, takeWhile, tap } from 'rxjs';
+import { combineLatest, concat, interval, map, Observable, take, takeWhile, tap } from 'rxjs';
 
 @Component({
   selector: 'app-progress-spinner',
@@ -12,11 +12,14 @@ export class ProgressSpinnerComponent implements OnInit {
   loadingPercent: number = 0;
   loadingWaitPercent: number = 0;
   mode: ProgressBarMode = "query"
+  loadingValueBuffer: number = 0;
+  bufferValue: number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
     this.loadingProgress(400).subscribe(i => this.loadingPercent = i);
+
     concat(
       interval(2000)
       .pipe(
@@ -25,6 +28,11 @@ export class ProgressSpinnerComponent implements OnInit {
       ),
       this.loadingProgress(500)
     ).subscribe(i => this.loadingWaitPercent = i);
+
+    combineLatest(this.loadingProgress(400), this.loadingProgress(300)).subscribe(i => {
+      this.loadingValueBuffer = i[0];
+      this.bufferValue = i[1]
+    });
   }
 
   loadingProgress(speed: number): Observable<number> {
